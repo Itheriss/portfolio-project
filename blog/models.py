24220ac33a11da03aadb15a85f26b230a225f818
George <george-rees@outllok.com>
2020-07-26 23:40:56 +0100

    added both main page and blog page, added postgres database and sorted static files

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     image =models.ImageField(upload_to="images/")
     body = models.TextField()
+    bodyWords = []
+    def summary(self):
+        self.bodyWords = self.body.split()
+        self.bodyWordsLim = self.bodyWords[:20]
+
+        return ' '.join(self.bodyWordsLim) + ' ...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'DjangoPass12',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -120,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
